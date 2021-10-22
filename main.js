fafb0610ef8a69c2d9544a87b4d480618b873834
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-10-22 15:42:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+video=""
+Status=true
 function preload() {
-    
+video=createVideo("video.mp4")
+video.hide()
 }
 function setup() {
-    canvas=createCanvas(380,380)
+    canvas=createCanvas(580,380)
     canvas.center()
 }
 function draw(){
+image(video,0,0,580,380)
 
+}
+function Start() {
+    ObjectDetector=ml5.objectDetector("COCOSSD",model_loaded)
+    document.getElementById("Status").innerHTML="Status:Detecting Objects"
+}
+function model_loaded() {
+    console.log("Model loaded");
+    Status=true
+    video.loop()
+    video.speed(1)
+    video.volume(0)
 }
