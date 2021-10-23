ae23bd8ffd250369a2d666c761dd52af0ed927ca
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-10-23 10:00:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video=""
 Status=true
+objects=[]
 function preload() {
 video=createVideo("video.mp4")
 video.hide()
@@ -8,12 +9,8 @@ function setup() {
     canvas=createCanvas(580,380)
     canvas.center()
 }
-function draw(){
-image(video,0,0,580,380)
-
-}
 function Start() {
-    ObjectDetector=ml5.objectDetector("COCOSSD",model_loaded)
+    objectDetector=ml5.objectDetector("cocossd",model_loaded)
     document.getElementById("Status").innerHTML="Status:Detecting Objects"
 }
 function model_loaded() {
@@ -22,4 +19,28 @@ function model_loaded() {
     video.loop()
     video.speed(1)
     video.volume(0)
+}
+function getResults(error,results) {
+    if (error) {
+        console.log(error)
+    }else{
+        console.log(results)
+    objects=results
+    }
+}
+function draw(){
+    image(video,0,0,580,380)
+    if (Status!="") {
+        objectDetector.detect(video,getResults)
+    for (let i = 0; i < objects.length; i++) {
+     document.getElementById("Status").innerHTML="Status: Objects detected"
+     document.getElementById("Number").innerHTML="Number of objects found:"+ objects.length
+     fill("#39a377")   
+    percent=floor(objects[i].confidence*100)
+    text(objects[i].label+"" +percent+ "%",objects[i].x +15,objects[i].y+15)
+    noFill()
+    stroke("#39a377")
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+    }
+    }
 }
